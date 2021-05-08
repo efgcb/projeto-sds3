1876d9753ff7ebb7444a1df2d3640046d05ecdbf
Eduardo Fernandes Gonçalves <efgcb@windowslive.com>
2021-05-08 11:21:20 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,23 @@
+import Dashboard from 'pages/Dashboard';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from 'pages/Home';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/dashboard">
+                    <Dashboard />
+                </Route>
+
+            </Switch>
+        
+        
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
